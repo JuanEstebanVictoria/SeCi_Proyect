66e17787f9a97937416e79691d13bbc222238686
Juan Victoria <juane.victorial@uqvirtual.edu.co>
2025-03-27 10:11:06 -0500

    Firt Commit

---
+++ src/main/java/co/edu/uniquindio/seci_proyect/SeCiProyectApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.seci_proyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeCiProyectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeCiProyectApplication.class, args);
+    }
+
+}
